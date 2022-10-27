1c8249c3f950ce27f0073d5aa80d4f5a4a430833
Skotij <sonja.kotijarvi@edu.keuda.fi>
2022-10-27 11:28:38 +0300

    27.10

---
+++ testaus/aritmeettiset.test.js
@@ -0,0 +1,16 @@
+//Yksilötestaukset
+const {summa, kerto, jako, vahennys} = require('./aritmeettiset')
+test('2 + 3 = 5', () => {
+  expect(summa(2,3)).toBe(5);})
+
+test('2 * 3 = 6', () => {
+  expect(kerto(2,3)).toBe(6);});
+
+  test('6 / 2 = 3', () => {
+    expect(jako(6,2)).toBe(3);});
+
+    test('6 - 2 = 4', () => {
+      expect(vahennys(6,2)).toBe(4);
+});
+//Joukkotestit
+test.each([[1, 1, 2], [-1, 2, 1],])
---
+++ testaus/summa.test.js
@@ -0,0 +1,5 @@
+const summa = require('./summa');
+test('2 + 3 = 5' ,( ) =>
+{
+  expect(summa(2,3)).toBe(5);
+})
